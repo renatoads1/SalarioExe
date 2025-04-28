8851bc038a7046189647096a10390eb9e7e58007
renatoads1 <renato.lacerda>
2025-04-28 14:25:02 -0300

    lista

---
+++ SalarioExe/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioExe
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+    }
+}
--- SalarioExe/Program.cs
+++ SalarioExe/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SalarioExe;
+int id = 0;
+List<Funcionario> funcionarios = new List<Funcionario>();
+
+for (int x = 0; x<3; x++) { 
+Console.WriteLine("Digite nome do funcionário ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Digite Salario do funcionário ");
+double salario = double.Parse(Console.ReadLine());
+    funcionarios.Add(new Funcionario
+    {
+        Id = id,
+        Nome = nome,
+        Salario = salario
+    });
+    id++;
+}
+Console.WriteLine(funcionarios[0].Nome);
+funcionarios.RemoveRange(1,1);
+Console.WriteLine(funcionarios[1].Nome);
+
+
